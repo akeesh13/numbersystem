088393516c3e907081d0d8064072b25e2367a13a
akeesh13 <akeeshm@gmail.com>
2024-12-24 20:37:45 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,7 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+  const odd = numbers.filter(num=>num%2!==0);
+  const even = numbers.filter(num=>num%2===0);
+  console.log(odd);
+  console.log(even);
